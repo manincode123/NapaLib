4a0e772d1161cdc120dfb44bd11f2bd3eaefd0b9
manincode123 <57838958+manincode123@users.noreply.github.com>
2019-11-16 23:44:35 +0700

    Bin and App_Start

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,77 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace NAPASTUDENT
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/SiteScript/Global.js"
+                ));
+            
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-validate/jquery.validate.min.js",
+                //"~/Scripts/jquery-validate/additional-methods.min.js",
+                "~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/moment-with-locales.js",
+                "~/Scripts/FontAwesome/all.js",
+                "~/Scripts/typeahead.js/typeahead.bundle.min.js",
+                "~/Scripts/select2/select2.min.js",
+                "~/Scripts/underscore-min.js"
+                //"~/Scripts/bootstrap4-editable/js/bootstrap-editable.min.js"   
+                 ));
+            bundles.Add(new ScriptBundle("~/bundles/DataTable").Include(
+                "~/Scripts/DataTables/jquery.dataTables.js",
+                "~/Scripts/DataTables/dataTables.bootstrap4.js",
+                "~/Scripts/DataTables/dataRender/datetime.js"
+            ));
+            
+            bundles.Add(new ScriptBundle("~/bundles/DataTablePlugin").Include(
+                "~/Scripts/DataTables/dataTables.buttons.js",
+                "~/Scripts/DataTables/buttons.bootstrap4.js",
+                "~/Scripts/DataTables/JSZip-2.5.0/jszip.js",
+                "~/Scripts/DataTables/pdfmake-0.1.36/pdfmake.js",
+                "~/Scripts/DataTables/pdfmake-0.1.36/vfs_fonts.js",
+                "~/Scripts/DataTables/buttons.flash.min.js",
+                "~/Scripts/DataTables/buttons.print.min.js",
+                "~/Scripts/DataTables/buttons.html5.min.js"
+                ));            
+            
+            bundles.Add(new ScriptBundle("~/bundles/Chart").Include(
+                "~/Scripts/Chart.js/Chart.min.js",
+                "~/Scripts/Chart.js/chartjs-plugin-datalabels.min.js"
+                ));
+
+            //"~/Scripts/FontAwesome/fontawesome.js",
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap4.css",
+                      "~/Content/DataTables/css/buttons.bootstrap4.css",
+                      "~/Content/sitecss/site.css",
+                      "~/Content/sitecss/ThanhDieuHuong.css",
+                      "~/Content/sitecss/BaiVietStyles.css",
+                      "~/Content/JTLine/css/style.css",
+                      "~/Content/typeahead/typeahead.css",
+                      "~/Content/select2/select2.min.css",
+                      "~/Content/bootstrap4-editable/css/bootstrap-editable.css" 
+                ));                                           
+        }
+    }
+}
---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Microsoft.Ajax.Utilities;
+using NAPASTUDENT.Controllers.Api;
+using NAPASTUDENT.Models;
+using NAPASTUDENT.Models.DTOs;
+using NAPASTUDENT.Models.DTOs.BaiVietDtos;
+using NAPASTUDENT.Models.DTOs.ChuyenMucDtos;
+using NAPASTUDENT.Models.DTOs.HoatDongDto;
+using NAPASTUDENT.Models.DTOs.LopDtos;
+using NAPASTUDENT.Models.DTOs.MonHocDtos;
+using NAPASTUDENT.Models.DTOs.SinhVienDto;
+
+namespace NAPASTUDENT.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<SinhVien, ChiTietDayDuSinhVienDto>()
+                .ForMember(svdto => svdto.TonGiao, opt => opt.MapFrom(sv => sv.TonGiao.TenTonGiao))
+                .ForMember(svdto => svdto.DanToc, opt => opt.MapFrom(sv => sv.DanToc.TenDanToc))
+                .ForMember(svdto => svdto.GioiTinh, opt => opt.MapFrom(sv => sv.GioiTinh.TenGioiTinh))
+                .ForMember(svdto => svdto.TenLop, opt => opt.MapFrom(sv => sv.LopDangHoc.KyHieuTenLop))
+                .ForMember(svdto => svdto.LopId, opt => opt.MapFrom(sv => sv.LopDangHoc.Id));
+                
+
+            CreateMap<DoanVien, HoiVienDoanVienDto>()
+                .ForMember(dvdto => dvdto.NoiVao, opt => opt.MapFrom(sv => sv.NoiVaoDoan))
+                .ForMember(dvdto => dvdto.NgayVao, opt => opt.MapFrom(sv => sv.NgayVaoDoan));
+
+            CreateMap<HoiVienHoiSinhVien, HoiVienDoanVienDto>()
+                .ForMember(hvdto => hvdto.NgayVao, opt => opt.MapFrom(sv => sv.NgayVaoHoi));
+
+            CreateMap<KhoaHoc, KhoaHocDto>();
+            CreateMap<KhoaHocDto, KhoaHoc>()
+                .ForMember(kh => kh.Id,opt => opt.Ignore());
+
+            CreateMap<SinhVien, SinhVienDtoForTable>()
+                .ForMember(svdto => svdto.GioiTinh, opt => opt.MapFrom(sv => sv.GioiTinh.TenGioiTinh));
+
+            CreateMap<SinhVien, TTSinhVienCBNhatDto>();
+
+            CreateMap<SinhVienSaveDto, SinhVien>()
+                .ForMember(sv => sv.Id, opt => opt.Ignore()) //Bỏ qua Id map Dto -> Entity
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+                
+                          
+            CreateMap<ChucVuLop, ChucVuLopDto>()
+                .ForMember(cvldto => cvldto.ChucVu, opt => opt.MapFrom(cvl => cvl.ChucVu.TenChucVu));
+
+            CreateMap<HdVaSoLuotTg, HdVaSoLuotTgDto>();
+
+            CreateMap<HoatDong, ThongTinCoBanHoatDongDto>();
+            CreateMap<HoatDong, HoatDongDtoForTable>();
+
+            CreateMap<DiaChi, DiaChiDto>()
+                .ForMember(diachiDto => diachiDto.CapTinh, opt => opt.MapFrom(diachi => diachi.CapTinh.TenTinh))
+                .ForMember(diachiDto => diachiDto.CapHuyen, opt => opt.MapFrom(diachi => diachi.CapHuyen.TenHuyen))
+                .ForMember(diachiDto => diachiDto.CapXa, opt => opt.MapFrom(diachi => diachi.CapXa.TenXa));
+            CreateMap<DiaChiDtoForSave, DiaChi>();
+
+            CreateMap<SDT, SDTDto>();
+            CreateMap<SDTDto, SDT>();
+
+            CreateMap<Lop, DanhSachLopDto>()
+                .ForMember(ldto => ldto.TenKhoaHoc,opt => opt.MapFrom(l => l.KhoaHoc.TenKhoa));
+
+            CreateMap<Lop, LopDto>()
+                .ForMember(ldto => ldto.KhoaHoc, opt => opt.MapFrom(l => l.KhoaHoc.TenKhoa));
+            //.ForMember(ldto => ldto.soLuongSV, opt => opt.MapFrom(l => l.KhoaHoc.DanhSachSinhVien.Count))
+            //.ForMember(ldto => ldto.soNu,
+            //    opt => opt.MapFrom(l => l.KhoaHoc.DanhSachSinhVien.Count(sv => sv.GioiTinhId == 1)))
+            //.ForMember(ldto => ldto.soNam,
+            //    opt => opt.MapFrom(l => l.KhoaHoc.DanhSachSinhVien.Count(sv => sv.GioiTinhId == 2)));
+
+            CreateMap<ChuongTrinhHoatDongDto, ChuongTrinhHoatDong>()
+                .ForMember(cthd => cthd.Id, opt => opt.Ignore()); 
+            
+            CreateMap<HoatDongDtoForSave, HoatDong>()
+                .ForMember(hd => hd.Id, opt => opt.Ignore())
+                .ForMember(hd => hd.DanhSachDonViToChuc, opt => opt.Ignore())
+                .ForMember(hd => hd.DanhSachLopToChuc, opt => opt.Ignore())
+                .ForMember(hd => hd.DaKetThuc, opt => opt.Ignore())
+                .ForMember(hd => hd.BiHuy, opt => opt.Ignore())
+                .ForMember(hd => hd.DuocPheDuyet, opt => opt.Ignore());
+
+            CreateMap<SaveLopDto, Lop>();
+            CreateMap<SaveBaiVietDto, BaiViet>();
+
+            CreateMap<ChuyenMucBaiViet, ChuyenMucSelectListDto>();
+            CreateMap<SaveChuyenMucDto, ChuyenMucBaiViet>();
+
+            CreateMap<MonHoc, MonHocDto>();
+            CreateMap<MonHocDto, MonHoc>();
+
+            CreateMap<DangKiMonHocDto,LopMonHoc>()
+                .ForMember(lmh => lmh.DanhSachLichHoc, opt => opt.Ignore());
+
+            CreateMap<LichHocDto, LichHoc>();
+
+            CreateMap<LichThiLai4SaveDto, LichThiLai>()
+                .ForMember(ltl => ltl.Id, opt => opt.Ignore());
+
+            CreateMap<ThongBaoHoatDong, ThongBaoHoatDongDto>()
+                .ForMember(tbDto => tbDto.TenHoatDong, opt => opt.MapFrom(tb => tb.HoatDong.TenHoatDong))
+                .ForMember(tbDto => tbDto.NgayBatDau, opt => opt.MapFrom(tb => tb.HoatDong.NgayBatDau))
+                .ForMember(tbDto => tbDto.NgayKetThuc, opt => opt.MapFrom(tb => tb.HoatDong.NgayKetThuc))
+                .ForMember(tbDto => tbDto.AnhBia, opt => opt.MapFrom(tb => tb.HoatDong.AnhBia))
+                .ForMember(tbDto => tbDto.DiaDiem, opt => opt.MapFrom(tb => tb.HoatDong.DiaDiem));
+
+            CreateMap<BaiVietResultSet, BaiVietSoLuocDto>();
+
+            CreateMap<ChucVuDonViDto, ChucVuDonVi>();
+            CreateMap<DonViDto, DonVi>()
+                .ForMember(dv => dv.Id, opt => opt.Ignore());
+
+        }
+    }
+}
